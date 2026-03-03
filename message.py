aac7d597a6544f159f068b6c23d6d19c6c5c78fd
Arsenii <senyailyin17@gmail.com>
2026-03-03 19:30:39 +0300

    Modules

---
+++ message.py
@@ -0,0 +1,20 @@
+# hello = "Hello all"
+#
+#
+# def print_message(text):
+#     print(f"Message: {text}")
+
+
+hello = "Hello all"
+
+
+def print_message(text):
+    print(f"Message: {text}")
+
+
+def main():
+    print_message(hello)
+
+
+if __name__ == "__main__":
+    main()
